6975831bffb1b50f64ea3144b330339c7a2a350d
DO-Ui <68296986+DO-Ui@users.noreply.github.com>
2022-03-19 12:34:44 -0400

    Changed code to monitor the correct pixels

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def watch(current):
         time.sleep(current/4)
         print("First point activated")
         while True:
-            sct_img = scan1.grab({'top': 1203, 'left':1160, 'width': 1, 'height':1})
+            sct_img = scan1.grab({'top': 940, 'left':1160, 'width': 1, 'height':1})
             img = Image.frombytes('RGB', (sct_img.size.width, sct_img.size.height), sct_img.rgb)
             point = img.getpixel((0,0))
             if point[0] == 194:
@@ -22,7 +22,7 @@ def watch(current):
         time.sleep(current/4)
         print("Second point activated")
         while True:
-            sct_img = scan2.grab({'top': 1203, 'left':1300, 'width': 1, 'height':1})
+            sct_img = scan2.grab({'top': 940, 'left':1300, 'width': 1, 'height':1})
             img = Image.frombytes('RGB', (sct_img.size.width, sct_img.size.height), sct_img.rgb)
             point = img.getpixel((0,0))
             if point[1] == 255:
@@ -32,7 +32,7 @@ def watch(current):
         time.sleep(current/4)
         print("Third point activated")
         while True:
-            sct_img = scan3.grab({'top': 1203, 'left':1440, 'width': 1, 'height':1})
+            sct_img = scan3.grab({'top': 940, 'left':1440, 'width': 1, 'height':1})
             img = Image.frombytes('RGB', (sct_img.size.width, sct_img.size.height), sct_img.rgb)
             point = img.getpixel((0,0))            
 
@@ -43,7 +43,7 @@ def watch(current):
         time.sleep(current/4)
         print("Last point activated")
         while True:
-            sct_img = scan4.grab({'top': 1203, 'left':1590, 'width': 1, 'height':1})
+            sct_img = scan4.grab({'top': 940, 'left':1590, 'width': 1, 'height':1})
             img = Image.frombytes('RGB', (sct_img.size.width, sct_img.size.height), sct_img.rgb)
             point = img.getpixel((0,0))
             if point[0] == 249:
